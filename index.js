1a69ce48abf9d66ad4c3ec6bf3ea1b3157c941b6
zaki <zz55555@gmail.com>
2026-03-16 22:18:30 +0500

    project updated

---
+++ index.js
@@ -0,0 +1,53 @@
+var canvas = document.getElementsByClassName('canvas')[0]
+var side = document.getElementsByClassName('side-menu')[0]
+
+const up_sections = document.querySelectorAll('.up_section');
+const left_sections = document.querySelectorAll('.left_section');
+const right_sections = document.querySelectorAll('.right_section');
+const process_box = document.querySelectorAll('.process_box');
+
+
+function sideMenu(type) {
+    if (type) {
+        console.log('helo')
+        canvas.style.display = 'block'
+        
+        side.style.transform = 'translateX(0px)'
+        
+    } else {
+        canvas.style.display = 'none'
+        
+        side.style.transform = 'translateX(100%)'
+    }
+
+}
+
+
+
+
+function up(entries) {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('up_animate');
+        }
+    });
+}
+function left(entries) {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('left_animate');
+        }
+    });
+}
+const up_observer = new IntersectionObserver((entries) => up(entries), { threshold: 0.3 });
+const left_observer = new IntersectionObserver((entries) => left(entries), { threshold: 0.5 });
+const right_observer = new IntersectionObserver((entries) => left(entries), { threshold: 0.5 });
+
+up_sections.forEach(section => up_observer.observe(section));
+left_sections.forEach(section => left_observer.observe(section));
+right_sections.forEach(section => right_observer.observe(section));
+
+
+
+
+
